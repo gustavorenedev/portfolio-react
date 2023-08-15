bbe5c550d96f0e53c81e4064fc8744fdfa9bd4fd
Gustavo René <renegustavo42@gmail.com>
2023-08-15 12:06:19 -0300

    Styled navbar

--- src/sections/navbar/Navbar.jsx
+++ src/sections/navbar/Navbar.jsx
@@ -1,9 +1,22 @@
+import Logo from '../../assets/logo.jpg'
+import data from './data'
+import {IoIosColorPalette} from 'react-icons/io'
 import './navbar.css'
 
 const Navbar = () => {
   return (
     <nav>
-        <h2>Navbar</h2>
+        <div className="container nav__container">
+          <a href="index.html" className="nav__logo">
+          &lt; <strong class="color">G</strong>r/ &gt;
+          </a>
+          <ul className="nav__menu">
+            {
+              data.map(item => <li key={item.id}><a href={item.link}>{item.title}</a></li>)
+            }
+          </ul>
+          <button id="theme__icon"><IoIosColorPalette/></button>
+        </div>
     </nav>
   )
 }
