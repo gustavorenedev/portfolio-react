a3df2d75216fd6c3cbeb6dd543de6aef9fe56e67
Gustavo René <renegustavo42@gmail.com>
2023-08-16 10:16:03 -0300

    update Header and styled about

--- src/sections/about/About.jsx
+++ src/sections/about/About.jsx
@@ -1,11 +1,51 @@
-import './about.css'
+import CV from "../../assets/CV-GustavoRené.pdf";
+import AboutImage from '../../assets/teste.jpg'
+import { AiOutlineDownload } from "react-icons/ai";
+import data from './data'
+import Card from '../../components/Card'
+import "./about.css";
 
 const About = () => {
   return (
     <section id="about">
-        <h2>About</h2>
+      <div className="container about__container">
+        <div className="about__left">
+            <div className="about__portrait">
+                <img src={AboutImage} alt="About Image" />
+            </div>
+        </div>
+        <div className="about__right">
+          <h2>Sobre mim</h2>
+          <div className="about__cards">
+              {
+                  data.map(item => (
+                      <Card key={item.id} className="about__card">
+                          <span className="about__card-icon">{item.icon}</span>
+                          <h5>{item.title}</h5>
+                          <small>{item.desc}</small>
+                      </Card>
+                  ))
+              }
+          </div>
+          <p>
+            Tenho 19 anos, sou técnico em TI pelo SENAC, e no momento curso
+            Análise e Desenvolvimento de Sistemas na FIAP. Possuo
+            disponibilidade de horários, perfil persistente para aprender novos
+            conceitos em pouco tempo e comprometimento para novas oportunidades.
+          </p>
+          <p>
+            Possuo conhecimentos na parte de Front-end: HTML, CSS, JavaScript,
+            Bootstrap e em Back-end: Java e MySQL. Atualmente sigo estudando as
+            tecnologias com foco em Back-end: React, JavaScript, NodeJS e
+            Python.
+          </p>
+          <a href={CV} download className="btn primary">
+            Download CV <AiOutlineDownload />
+          </a>
+        </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default About
+export default About;
---
+++ src/sections/about/data.js
@@ -0,0 +1,10 @@
+import {FaAward} from 'react-icons/fa'
+import {TbBooks} from 'react-icons/tb'
+import {BiHappyHeartEyes} from 'react-icons/bi'
+
+const data = [
+    {id: 1, icon: <FaAward/>, title: 'Experiência', desc: '1 ano de estudos'},
+    {id: 2, icon: <TbBooks/>, title: 'Projetos', desc: '10+ completos'},
+]
+
+export default data;
