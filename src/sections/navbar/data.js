47d3b7abf82a8b8371570b08ca9cdf7fef973b51
Gustavo René <renegustavo42@gmail.com>
2023-08-17 10:58:24 -0300

    Update navbar

--- src/sections/navbar/data.js
+++ src/sections/navbar/data.js
@@ -1,9 +1,9 @@
 const data = [
-    {id: 1, link: "#", title: "Home"},
-    {id: 2, link: "#about", title: "About"},
-    {id: 3, link: "#skills", title: "Skills"},
-    {id: 4, link: "#portfolio", title: "Portfolio"},
-    {id: 5, link: "#contact", title: "Contact"},
+    {id: 1, link: "#", title: "Início"},
+    {id: 2, link: "#about", title: "Sobre"},
+    {id: 3, link: "#skills", title: "Habilidades"},
+    {id: 4, link: "#portfolio", title: "Projetos"},
+    {id: 5, link: "#contact", title: "Contato"},
 ]
 
 export default data;
