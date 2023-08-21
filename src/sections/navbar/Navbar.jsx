2a571a5107e96f34f7eb2f0c1cf7f8670b7f99b7
Gustavo René <renegustavo42@gmail.com>
2023-08-21 09:52:33 -0300

    Update styling

--- src/sections/navbar/Navbar.jsx
+++ src/sections/navbar/Navbar.jsx
@@ -1,9 +1,10 @@
 import data from './data'
-import {IoIosColorPalette} from 'react-icons/io'
-import {showModalContext} from '../../context/modal-context'
+import {useModalContext} from '../../context/modal-context'
 import './navbar.css'
 
 const Navbar = () => {
+  const {showModalHandler} = useModalContext();
+
   return (
     <nav>
         <div className="container nav__container">
@@ -15,7 +16,7 @@ const Navbar = () => {
               data.map(item => <li key={item.id}><a href={item.link}>{item.title}</a></li>)
             }
           </ul>
-          <button id="theme__icon"><IoIosColorPalette/></button>
+          <div></div>
         </div>
     </nav>
   )
