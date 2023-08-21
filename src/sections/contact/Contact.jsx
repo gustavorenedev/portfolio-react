6d1a4ccaf5ef84e8315a300b07592e416981129d
Gustavo René <renegustavo42@gmail.com>
2023-08-21 09:51:59 -0300

    Update styling

--- src/sections/contact/Contact.jsx
+++ src/sections/contact/Contact.jsx
@@ -6,7 +6,7 @@ const Contact = () => {
     <section id="contact">
         <h2>Entre em contato</h2>
         <p>Envie-me uma mensagem através de qualquer um dos links abaixo</p>
-    <div className="container contact__container">
+    <div className="container contact__container" data-aos="fade-up">
       {
         contacts.map(contact => <a key={contact.id} href={contact.link} target='_blank' 
         rel='noopener noreferrer'>{contact.icon}</a>)
