32014309bb27beb9a1e6d475cbc009ebbb62ab26
Gustavo René <renegustavo42@gmail.com>
2023-08-21 09:52:14 -0300

    Update styling

--- src/sections/header/Header.jsx
+++ src/sections/header/Header.jsx
@@ -1,20 +1,27 @@
+import { useEffect } from 'react'
 import HeaderImage from '../../assets/fotopessoal.jpeg'
 import data from './data'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
 import './header.css'
 
 const Header = () => {
+  useEffect(() => {
+    AOS.init({duration:2000})
+  }, [])
+
   return (
     <header id="header">
         <div className="container header__container">
-          <div className="header__profile">
+          <div className="header__profile" data-aos="fade-in">
             <img src={HeaderImage} alt="personal photo" />
           </div>
-          <h3>Gustavo René</h3>
-          <p>
+          <h3 data-aos="fade-up">Gustavo René</h3>
+          <p data-aos="fade-up">
           Olá! Sou Gustavo René, desenvolvedor Back-end com especialidade em JavaScript, NodeJS e SQL. 
           Ajudo pequenos negócios e designers a colocarem em prática boas ideias. 
           </p>
-          <div className="header__cta">
+          <div className="header__cta" data-aos="fade-up">
             <a href="#contact" className='btn primary'>Vamos conversar ?</a>
             <a href="#portfolio" className='btn light'>Meu trabalho</a>
           </div>
