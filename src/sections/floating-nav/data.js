9e11c8719def4c41c31af271f467453fd634d1bf
Gustavo René <renegustavo42@gmail.com>
2023-08-22 09:44:28 -0300

    Updated icons floating-nav

--- src/sections/floating-nav/data.js
+++ src/sections/floating-nav/data.js
@@ -1,15 +1,15 @@
 import {AiFillHome} from 'react-icons/ai'
 import {BsPersonFill} from 'react-icons/bs'
-import {MdHomeRepairService} from 'react-icons/md'
-import {AiFillAppstore} from 'react-icons/ai'
+import {BsCodeSlash} from 'react-icons/bs'
+import {ImBooks} from 'react-icons/im'
 import {AiFillMessage} from 'react-icons/ai'
 
 
 const data = [
     {id: 1, link: '#', icon: <AiFillHome/>},
     {id: 2, link: '#about', icon: <BsPersonFill/>},
-    {id: 3, link: '#skills', icon: <MdHomeRepairService/>},
-    {id: 4, link: '#portfolio', icon: <AiFillAppstore/>},
+    {id: 3, link: '#skills', icon: <BsCodeSlash/>},
+    {id: 4, link: '#portfolio', icon: <ImBooks/>},
     {id: 5, link: '#contact', icon: <AiFillMessage/>}
 ]
 
