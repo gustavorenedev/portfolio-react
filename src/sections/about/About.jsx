383bbd329b14d6b5999af2074da04e67cd605b60
Gustavo René <renegustavo42@gmail.com>
2023-08-24 18:31:01 -0300

    Updated about

--- src/sections/about/About.jsx
+++ src/sections/about/About.jsx
@@ -1,8 +1,8 @@
-import CV from "../../assets/CV-GustavoRené.pdf";
-import AboutImage from '../../assets/fotopessoal.jpeg'
+import CV from "../../assets/CV-GustavoRene.pdf";
+import AboutImage from "../../assets/fotopessoal.jpeg";
 import { AiOutlineDownload } from "react-icons/ai";
-import data from './data'
-import Card from '../../components/Card'
+import data from "./data";
+import Card from "../../components/Card";
 import "./about.css";
 
 const About = () => {
@@ -10,35 +10,38 @@ const About = () => {
     <section id="about" data-aos="fade-in">
       <div className="container about__container">
         <div className="about__left">
-            <div className="about__portrait">
-                <img src={AboutImage} alt="About Image" />
-            </div>
+          <div className="about__portrait">
+            <img src={AboutImage} alt="About Image" />
+          </div>
         </div>
         <div className="about__right">
           <h2>Sobre mim</h2>
           <div className="about__cards">
-              {
-                  data.map(item => (
-                      <Card key={item.id} className="about__card">
-                          <span className="about__card-icon">{item.icon}</span>
-                          <h5>{item.title}</h5>
-                          <small>{item.desc}</small>
-                      </Card>
-                  ))
-              }
+            {data.map((item) => (
+              <Card key={item.id} className="about__card">
+                <span className="about__card-icon">{item.icon}</span>
+                <h5>{item.title}</h5>
+                <small>{item.desc}</small>
+              </Card>
+            ))}
           </div>
           <p>
-          Com 19 anos, sou técnico em TI pelo SENAC e estudo Análise e Desenvolvimento de Sistemas na FIAP. 
-          Minha flexibilidade de horários me permite abraçar novas oportunidades de aprendizado. 
-          Tenho determinação para absorver conceitos rapidamente e encarar desafios com comprometimento.
+            Com 20 anos, sou técnico em TI pelo SENAC e estudo Análise e
+            Desenvolvimento de Sistemas na FIAP. Minha flexibilidade de horários
+            me permite abraçar novas oportunidades de aprendizado. Tenho
+            determinação para absorver conceitos rapidamente e encarar desafios
+            com comprometimento.
           </p>
           <p>
-          Em Front-end, domino HTML, CSS, JavaScript e Bootstrap, enquanto em Back-end, possuo experiência em NodeJS, Java, 
-          Python, JavaScript e banco de dados SQL e NoSQL para fortalecer meu repertório técnico.
+            Em Front-end, domino HTML, CSS, JavaScript e Bootstrap, enquanto em
+            Back-end, possuo experiência em NodeJS, Java, Python, JavaScript e
+            banco de dados SQL e NoSQL para fortalecer meu repertório técnico.
           </p>
           <p>
-          Tenho muita facilidade no desenvolvimento web, especialmente no back-end, possuo habilidades nessa área e 
-          busco contribuir em projetos inovadores, criando soluções sólidas para as demandas do mercado.
+            Tenho muita facilidade no desenvolvimento web, especialmente no
+            back-end, possuo habilidades nessa área e busco contribuir em
+            projetos inovadores, criando soluções sólidas para as demandas do
+            mercado.
           </p>
           <a href={CV} download className="btn primary">
             Download CV <AiOutlineDownload />
