ed431093a9fa16a75400479e499a6d7218272849
Gustavo René <renegustavo42@gmail.com>
2024-07-27 14:17:27 -0300

    chore: Updating about me

--- src/sections/about/About.jsx
+++ src/sections/about/About.jsx
@@ -26,22 +26,22 @@ const About = () => {
             ))}
           </div>
           <p>
-            Olá, sou Gustavo! Atualmente, sou desenvolvedor .NET Júnior e
-            estudante de Análise e Desenvolvimento de Sistemas na FIAP. Moro em
-            São Paulo, capital, e estou à disposição para ajudar a transformar
-            suas ideias em soluções incríveis.
+            Olá, sou Gustavo! Atualmente, sou desenvolvedor .NET Júnior na
+            Redeservice e estudante de Análise e Desenvolvimento de Sistemas na
+            FIAP. Moro em São Paulo, capital, e estou à disposição para ajudar a
+            transformar suas ideias em soluções incríveis.
           </p>
           <p>
             Minhas habilidades abrangem diversas áreas: <br />
-            <strong>Front-end:</strong> React com JavaScript e TypeScript.{" "}
+            <strong>Front-end:</strong> React com JavaScript e TypeScript.
             <br />
-            <strong>Back-end:</strong> C#, Java e frameworks como .NET Core e
+            <strong>Back-end:</strong> C#, Java, e frameworks como .NET Core e
             Spring Boot. <br />
-            <strong>Banco de Dados:</strong> PL/SQL, SQL com SQL Server, MySQL e
-            Oracle SQL. <br />
+            <strong>Banco de Dados:</strong> PL/SQL, SQL (SQL Server, MySQL e
+            Oracle SQL). <br />
             <strong>Arquitetura de Projetos:</strong> API Rest, Microsserviços,
-            RabbitMQ, MVC, Repository. <br />
-            <strong>Controle de Versão:</strong> Git e GitHub e Azure DevOps.
+            RabbitMQ, MVC, e Repository. <br />
+            <strong>Controle de Versão:</strong> Git, GitHub e Azure DevOps.
           </p>
 
           <a href={CV} download className="btn primary">
